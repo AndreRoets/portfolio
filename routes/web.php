32c04e9afa0454f2416e3c2b474ca729daa88d0a
AndreRoets <a.roets12@gmail.com>
2025-10-26 20:55:24 +0200

    portfo;io page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
-    return view('home'); // This was 'welcome', I've corrected it to 'home'
-});
+    return view('home');
+})->name('home');
 
 Route::get('/portfolio', function () {
-    return view('portfolio');
-});
+    return view('pages.portfolio');
+})->name('portfolio');
+
+Route::get('/portfolio/web', function () {
+    return view('pages.portfolio');
+})->name('portfolio.web');
+
+Route::get('/portfolio/design', function () {
+    return view('pages.portfolio');
+})->name('portfolio.design');
